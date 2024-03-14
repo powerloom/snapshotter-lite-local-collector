8cf98d69cc6cbe945419a00eb2643b6db23386d4
muku314115 <mukangotan@gmail.com>
2024-03-14 16:00:10 +0530

    logging

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"proto-snapshot-server/config"
+	"proto-snapshot-server/pkgs/helpers"
 	"proto-snapshot-server/pkgs/service"
 	"sync"
 )
 
 func main() {
 	var wg sync.WaitGroup
+	helpers.InitLogger()
 	config.LoadConfig()
 	service.ConfigureRelayer()
 	wg.Add(1)
