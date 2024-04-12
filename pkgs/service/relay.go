a72b3baad4596ebb6c784fb38ed8f80182664cb5
muku314115 <mukangotan@gmail.com>
2024-04-12 12:56:22 +0530

    tried and tested changes - finalizing code

--- pkgs/service/relay.go
+++ pkgs/service/relay.go
@@ -40,65 +40,12 @@ func ConfigureRelayer() {
 		libp2p.EnableRelayService(),
 		libp2p.EnableNATService())
 
-	//// Set up a Kademlia DHT for the service host
-	//kademliaDHT, err := dht.New(context.Background(), rpctorelay)
-	//if err != nil {
-	//	log.Fatalf("Failed to create DHT: %s", err)
-	//}
-	//
-	//// Bootstrap the DHT
-	//if err = kademliaDHT.Bootstrap(context.Background()); err != nil {
-	//	log.Fatalf("Failed to bootstrap DHT: %s", err)
-	//}
-	//
-	//var wg sync.WaitGroup
-	//for _, peerAddr := range dht.DefaultBootstrapPeers {
-	//	peerinfo, _ := peer.AddrInfoFromP2pAddr(peerAddr)
-	//	wg.Add(1)
-	//	go func() {
-	//		defer wg.Done()
-	//		if err := rpctorelay.Connect(context.Background(), *peerinfo); err != nil {
-	//			log.Warning(err)
-	//		} else {
-	//			log.Debugln("Connection established with bootstrap node:", *peerinfo)
-	//		}
-	//	}()
-	//}
-	//wg.Wait()
+	// Set up a Kademlia DHT for the service host
 
 	kademliaDHT := ConfigureDHT(context.Background(), rpctorelay)
 
 	routingDiscovery = routing.NewRoutingDiscovery(kademliaDHT)
 	peerId := ConnectToPeer(context.Background(), routingDiscovery, config.SettingsObj.RendezvousPoint, rpctorelay)
-	//// Find peers advertising under the same rendezvous string
-	//peerChan, err := routingDiscovery.FindPeers(context.Background(), config.SettingsObj.RendezvousPoint)
-	//if err != nil {
-	//	log.Fatalf("Failed to find peers: %s", err)
-	//}
-	//
-	//var peerId string
-	//
-	//for peer := range peerChan {
-	//	if peer.ID == rpctorelay.ID() || len(peer.Addrs) == 0 {
-	//		// Skip self or peers with no addresses
-	//		continue
-	//	}
-	//
-	//	log.Debugf("Found peer: %s/%s\n", peer.Addrs[0], peer.ID.String())
-	//
-	//	// Connect to the peer
-	//	if err := rpctorelay.Connect(context.Background(), peer); err != nil {
-	//		log.Printf("Failed to connect to peer %s: %s", peer.ID.String(), err)
-	//	} else {
-	//		if err = rpctorelay.Connect(ctx, peer); err != nil {
-	//			log.Debugln("Failed to connect grpc server to relayer")
-	//		}
-	//		peerId = peer.ID.String()
-	//
-	//		log.Debugf("Connected to peer: %s\n", peer.ID.String())
-	//		break
-	//	}
-	//}
 
 	var collectorAddr ma.Multiaddr
 
