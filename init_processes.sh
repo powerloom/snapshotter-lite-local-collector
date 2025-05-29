1016121762964bc8812a7604de2c6982f35afb67
Akshay Dahiya <akshay@powerloom.io>
2025-05-29 08:29:21 +0700

    chore: Add exit trap

--- init_processes.sh
+++ init_processes.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+handle_exit() {
+    EXIT_CODE=$?
+    
+    echo "Container exited with code $EXIT_CODE. Restarting..."
+    exit 1
+}
+
+trap 'handle_exit' EXIT HUP INT QUIT ABRT TERM
+
 echo 'starting pm2...';
 
 pm2 start pm2.config.js
