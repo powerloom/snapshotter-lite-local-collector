bc5ba25bb8fd299499fb28d0f5d48fccc0ed4d83
anomit ghosh <anomit.ghosh@gmail.com>
2025-08-06 18:48:07 +0530

    feat: enhance logging functionality by adding support for configurable log file output and improved error handling

--- pkgs/helpers/logger.go
+++ pkgs/helpers/logger.go
@@ -6,31 +6,59 @@ import (
 	"github.com/sirupsen/logrus/hooks/writer"
 	"io"
 	"os"
+	"path/filepath"
 	"strconv"
 )
 
 func InitLogger() {
-	log.SetOutput(io.Discard) // Send all logs to nowhere by default
+	// Check if LOG_FILE environment variable is set
+	logFile := os.Getenv("LOG_FILE")
+	if logFile != "" {
+		// Ensure log directory exists
+		logDir := filepath.Dir(logFile)
+		if err := os.MkdirAll(logDir, 0755); err != nil {
+			fmt.Printf("Failed to create log directory: %v\n", err)
+			// Fall back to stdout/stderr
+			logFile = ""
+		} else {
+			// Open log file
+			file, err := os.OpenFile(logFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+			if err != nil {
+				fmt.Printf("Failed to open log file: %v\n", err)
+				// Fall back to stdout/stderr
+				logFile = ""
+			} else {
+				// Write to both file and stdout/stderr
+				log.SetOutput(io.MultiWriter(file, os.Stdout))
+			}
+		}
+	}
+
+	// If no log file or failed to open, use default behavior
+	if logFile == "" {
+		log.SetOutput(io.Discard) // Send all logs to nowhere by default
+
+		log.AddHook(&writer.Hook{ // Send logs with level higher than warning to stderr
+			Writer: os.Stderr,
+			LogLevels: []log.Level{
+				log.PanicLevel,
+				log.FatalLevel,
+				log.ErrorLevel,
+				log.WarnLevel,
+			},
+		})
+		log.AddHook(&writer.Hook{ // Send info and debug logs to stdout
+			Writer: os.Stdout,
+			LogLevels: []log.Level{
+				log.TraceLevel,
+				log.InfoLevel,
+				log.DebugLevel,
+			},
+		})
+	}
 
 	log.SetReportCaller(true)
 
-	log.AddHook(&writer.Hook{ // Send logs with level higher than warning to stderr
-		Writer: os.Stderr,
-		LogLevels: []log.Level{
-			log.PanicLevel,
-			log.FatalLevel,
-			log.ErrorLevel,
-			log.WarnLevel,
-		},
-	})
-	log.AddHook(&writer.Hook{ // Send info and debug logs to stdout
-		Writer: os.Stdout,
-		LogLevels: []log.Level{
-			log.TraceLevel,
-			log.InfoLevel,
-			log.DebugLevel,
-		},
-	})
 	if len(os.Args) < 2 {
 		fmt.Println("Pass loglevel as an argument if you don't want default(INFO) to be set.")
 		fmt.Println("Values to be passed for logLevel: ERROR(2),INFO(4),DEBUG(5)")
