b7272817cf3f2645cc1bcfda048431f1c3ef60dd
anomit <anomit.ghosh@gmail.com>
2026-03-22 14:24:46 +0530

    chore: remove logging for filtered reserved IP addresses in RFC1918 filter

--- pkgs/service/rfc1918_filter.go
+++ pkgs/service/rfc1918_filter.go
@@ -8,7 +8,6 @@ import (
 	"github.com/libp2p/go-libp2p/core/network"
 	"github.com/libp2p/go-libp2p/core/peer"
 	ma "github.com/multiformats/go-multiaddr"
-	log "github.com/sirupsen/logrus"
 )
 
 // Reserved IP ranges that should be blocked
@@ -69,10 +68,6 @@ func FilterRFC1918Multiaddrs(addrs []ma.Multiaddr) ([]ma.Multiaddr, int) {
 		}
 
 		filtered = append(filtered, addr)
-	}
-
-	if filteredCount > 0 {
-		log.Infof("Filtered %d reserved IP addresses from peer addresses (RFC1918/RFC6598/RFC2544)", filteredCount)
 	}
 
 	return filtered, filteredCount
